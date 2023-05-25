5f892674c8d2d6ffae6203c239d91f0125de047e
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-05-25 20:47:17 -0300

    Feat - created some comments

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import fastify from "fastify";
 import { authRoutes } from "./routes/auth";
 import { errorHandler } from "./error/errorHandler";
+import fastifyJwt from "@fastify/jwt";
 
 const app = fastify()
 
 
+app.register(fastifyJwt, { secret: "b4d secr3T!123@@" })
+
 app.register(authRoutes)
 app.setErrorHandler(errorHandler)
+
+// app.addHook("onRequest", (request, reply, done) => {
+//   console.log("oi")
+//   const path = request.routerPath
+//   console.log({ path })
+//   if (path === "/auth/logout")
+//     return reply.status(httpStatus.UNAUTHORIZED).send({ massage: "flw" })
+//   done()
+// })
+
 
 app.listen({
   port: 3333
