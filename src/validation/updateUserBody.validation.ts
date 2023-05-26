c064b93da528f3783f8ca8749a0fb331fc0e375f
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-05-26 20:37:45 -0300

    Fix - fixed validation

--- src/validation/updateUserBody.validation.ts
+++ src/validation/updateUserBody.validation.ts
@@ -5,7 +5,7 @@ import { UpdateUser } from "../interface";
 export const validateUpdateUserBodySchema = (data: unknown): UpdateUser => {
   const bodySchema = z.object({
     name: z.string(),
-    phoneNumber: z.string()
+    phoneNumber: z.string().length(11)
   })
 
   const loginData = bodySchema.parse(data)
