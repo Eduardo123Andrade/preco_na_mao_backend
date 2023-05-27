516ee5e379c9b1cccc4ca8bb24cdc78f12a1ec38
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-05-26 22:55:16 -0300

    Feat - created marketplace list router

---
+++ src/routes/marketplace.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify";
+import { MarketplaceListController } from "../controller";
+
+
+
+export const marketplaceRoutes = async (app: FastifyInstance) => {
+  app.get("/marketplace-list", MarketplaceListController.getAllList)
+  app.get("/marketplace-list/:marketplaceListId", MarketplaceListController.getListById)
+
+}
--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,16 @@ import { errorHandler } from "./error/errorHandler";
 import fastifyJwt from "@fastify/jwt";
 import { onRequestHook } from "./hooks/onRequest";
 import { userRoutes } from "./routes/user";
+import { marketplaceRoutes } from "./routes/marketplace";
 
 const app = fastify()
 
 
 app.register(fastifyJwt, { secret: "b4d secr3T!123@@" })
 
-app.register(userRoutes)
 app.register(authRoutes)
+app.register(userRoutes)
+app.register(marketplaceRoutes)
 
 app.setErrorHandler(errorHandler)
 
