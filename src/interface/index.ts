24912da616b008c0814be692318b4df4174924b9
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-05-26 21:02:03 -0300

    Created update password interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,3 +1,4 @@
 export { SingUp } from "./singUp.interface"
 export { Login } from "./login.interface"
+export { UpdatePassword } from "./updatePassword.interface"
 export { UpdateUser } from "./updateUser.interface"
