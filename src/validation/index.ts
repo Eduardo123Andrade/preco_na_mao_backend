f113dcbd8eeb637e8176c40fb96204ab63a1f74c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-05-26 21:02:24 -0300

    Feat - created update password body schema validation

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,3 +1,4 @@
 export { validateLoginBodySchema } from "./loginBody.validation"
 export { validateSingUpSchemaBody } from "./singUpBody.validation"
+export { validateUpdatePasswordBodySchema } from "./updatePasswordBody.validation"
 export { validateUpdateUserBodySchema } from "./updateUserBody.validation"
