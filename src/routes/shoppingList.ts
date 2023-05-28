273a844547bc0a48498198ced1eedafee0a858f2
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-05-28 20:07:15 -0300

    Feat - created router to delete shopping list

--- src/routes/shoppingList.ts
+++ src/routes/shoppingList.ts
@@ -7,6 +7,8 @@ export const shoppingListRoutes = async (app: FastifyInstance) => {
   app.get("/shopping-list", ShoppingListController.getAllList)
   app.get("/shopping-list/:shoppingListId", ShoppingListController.getListById)
 
+  app.delete("/shopping-list/:shoppingListId", ShoppingListController.deleteShoppingList)
+
   app.post("/shopping-list", ShoppingListController.create)
   app.put("/shopping-list", ShoppingListController.insertItemsOnShoppingList)
 }
